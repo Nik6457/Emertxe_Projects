8759288ef6ebdd060f484a4d1488c5a70620b609
Nikhil Paikrao <Niksofficial4me@gmail.com>
2024-11-10 02:15:02 +0530

    Add files via upload

---
+++ CAR_BLACK_BOX/build/default/production/external_eep.p1.d
@@ -0,0 +1,12 @@
+build/default/production/external_eep.p1:  \
+external_eep.c  \
+main.h  \
+adc.h  \
+clcd.h  \
+ds1307.h  \
+i2c.h  \
+digital_keypad.h  \
+car_black_box.h  \
+external_eep.h  \
+timers.h  \
+uart.h 
---
+++ CAR_BLACK_BOX/build/default/production/isr.p1.d
@@ -0,0 +1,12 @@
+build/default/production/isr.p1:  \
+isr.c  \
+main.h  \
+adc.h  \
+clcd.h  \
+ds1307.h  \
+i2c.h  \
+digital_keypad.h  \
+car_black_box.h  \
+external_eep.h  \
+timers.h  \
+uart.h 
